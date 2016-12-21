85109c9e19adbc7993e9e4804f49f2e6e087f8bb
Rob Moore <rob@vynyl.com>
2016-12-21 14:08:33 -0700

    Add more styling changes, setup create repo button functionality

--- static/index.js
+++ static/index.js
@@ -1,4 +1,34 @@
 'use strict';
 
-const userInput = document.getElementById('repoBox').value;
-console.log(userInput);
+function createRepo() {
+  const userInput = document.getElementById('repoBox').value;
+  // TODO: Variable for github username
+  const privateBox = document.getElementById('privateBox');
+  const payload = {
+    name: `${userInput}`,
+    homepage: 'https://github.com',
+    auto_init: true,
+    private: false,
+    has_issues: false,
+    has_wiki: false,
+    has_downloads: true,
+  };
+  if (privateBox.checked === true) {
+    payload.private = true;
+    return true;
+  }
+}
+//   fetch(`//api.github.com/${userInput}`, { method: 'POST' })
+//       .then(response => {
+//         if (response.status >= 400) {
+//           // TODO: Do I need to pass this error to next()?
+//           throw new Error('Bad response from server');
+//         }
+//         return response.json();
+//       })
+//       .then(stories => {
+//         res.send(stories);
+//         return next();
+//       })
+//       .catch(next);
+// }
