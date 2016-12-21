6f287395bb11f5091e77095cbb00481537a60107
Rob Moore <rob@vynyl.com>
2016-12-20 17:31:23 -0700

    Add isomorphic-fetch, bulma and wrote first fetch

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const config = require('../config');
 const nodegit = require('../middleware/nodegit');
 const express = require('express');
+const fetch = require('isomorphic-fetch');
 
 const app = module.exports = express();
 app.locals.config = config;
 
 app.use('/', express.static('static'));
-app.get('/test', (req, res, next) => {
-  res.send('hi from test');
-  next();
+
+app.use('/test', (req, res, next) => {
+  fetch('//offline-news-api.herokuapp.com/stories')
+      .then(response => {
+        if (response.status >= 400) {
+          // TODO: Do I need to pass this error to next()?
+          throw new Error('Bad response from server');
+        }
+        return response.json();
+      })
+      .then(stories => {
+        res.send(stories);
+        return next();
+      })
+      .catch(next);
 });
+
+
 app.use(nodegit());
 
 app.listen(config.get('port'), config.get('ip'), () => {
---
+++ static/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const userInput = document.getElementById('repoBox').value;
+console.log(userInput);
