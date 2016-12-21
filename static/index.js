0a3e5bcb271922b641d0e1c6efb106fa83428c82
Rob Moore <rob@vynyl.com>
2016-12-21 14:48:09 -0700

    added comments outlining what needs to happen with form

--- static/index.js
+++ static/index.js
@@ -2,7 +2,8 @@
 
 function createRepo() {
   const userInput = document.getElementById('repoBox').value;
-  // TODO: Variable for github username
+  // TODO: Variable for github username through Auth
+
   const privateBox = document.getElementById('privateBox');
   const payload = {
     name: `${userInput}`,
@@ -13,11 +14,48 @@ function createRepo() {
     has_wiki: false,
     has_downloads: true,
   };
+
   if (privateBox.checked === true) {
     payload.private = true;
     return true;
   }
+  console.log(payload);
 }
+
+// TODO: Run GET command from postman and put values into variables
+// https://api.github.com/repos/rob-moore/postmanTest/git/refs/head
+
+// TODO: Run POST command to add development branch
+// https://api.github.com/repos/rob-moore/postmanTest/git/refs
+// {
+//     "ref": "refs/heads/development",
+//     "sha": "HASH FROM GET"
+// }
+
+// TODO: Run PATCH command from postman with custom variables to make development branch default
+// https://api.github.com/repos/rob-moore/postmanTest
+// {
+// "name": `${userInput}`,
+// "default_branch": "development"
+// }
+
+// TODO: Run PUT command from postman to add branch protection
+// https://api.github.com/repos/rob-moore/postmanTest/branches/development/protection
+// {
+//   "required_status_checks": {
+//     "include_admins": true,
+//     "strict": true,
+//     "contexts": [
+//       "continuous-integration/travis-ci" Probably something different here
+//     ]
+//   },
+//   "required_pull_request_reviews": {
+//     "include_admins": false
+//   },
+//   "restrictions": null
+//    This will NOT be null, fill it in with organization and teams
+// }
+
 //   fetch(`//api.github.com/${userInput}`, { method: 'POST' })
 //       .then(response => {
 //         if (response.status >= 400) {
