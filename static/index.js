3304cce51653939deaa464fabc9be6a70810dad9
Rob Moore <rob@vynyl.com>
2016-12-22 11:38:00 -0700

    Begin laying out JS fetch requests on the github api

--- static/index.js
+++ static/index.js
@@ -1,72 +1,91 @@
 'use strict';
 
 function createRepo() {
-  const userInput = document.getElementById('repoBox').value;
+  const repoName = document.getElementById('repoBox').value;
   // TODO: Variable for github username through Auth
 
   const privateBox = document.getElementById('privateBox');
   const payload = {
-    name: `${userInput}`,
-    homepage: 'https://github.com',
+    name: `${repoName}`,
+    homepage: 'https://vynyl.com',
     auto_init: true,
-    private: false,
+    private: true,
     has_issues: false,
     has_wiki: false,
     has_downloads: true,
   };
 
-  if (privateBox.checked === true) {
-    payload.private = true;
+  if (privateBox.checked === false) {
+    payload.private = false;
     return true;
   }
-  console.log(payload);
+  fetch(`//api.github.com/${userName}/repos`, {
+    method: 'POST',
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    body: payload,
+  })
+  .then(response => {
+    if (response.status <= 400) {
+      throw new Error('Server responded with error < 400');
+    }
+  })
+  .catch(next);
 }
 
 // TODO: Run GET command from postman and put values into variables
-// https://api.github.com/repos/rob-moore/postmanTest/git/refs/head
+fetch(`//api.github.com/repos/${userName}/${repoName}/git/refs/head`, {
+  method: 'GET',
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+})
+.then(response => {
+  if (response.status <= 400) {
+    throw new Error('Server responded with error < 400');
+  }
+  const sha = response.[0].object.sha;
+  return sha;
+})
+  .catch(next);
 
 // TODO: Run POST command to add development branch
-// https://api.github.com/repos/rob-moore/postmanTest/git/refs
-// {
-//     "ref": "refs/heads/development",
-//     "sha": "HASH FROM GET"
-// }
+fetch(`//api.github.com/repos/${userName}/${repoName}/git/refs`, {
+  method: 'POST',
+  'Content-Type': 'application/json',
+  body: {
+    ref: 'refs/heads/development',
+    sha: sha,
+  },
+})
+
 
 // TODO: Run PATCH command from postman with custom variables to make development branch default
-// https://api.github.com/repos/rob-moore/postmanTest
-// {
-// "name": `${userInput}`,
-// "default_branch": "development"
-// }
+fetch(`//api.github.com/repos/${userName}/${repoName}`, {
+  method: 'PATCH',
+  'Content-Type': 'application/json',
+  body: {
+    name: `${repoName}`,
+    default_branch: 'development',
+  },
+});
+
 
 // TODO: Run PUT command from postman to add branch protection
-// https://api.github.com/repos/rob-moore/postmanTest/branches/development/protection
-// {
-//   "required_status_checks": {
-//     "include_admins": true,
-//     "strict": true,
-//     "contexts": [
-//       "continuous-integration/travis-ci" Probably something different here
-//     ]
-//   },
-//   "required_pull_request_reviews": {
-//     "include_admins": false
-//   },
-//   "restrictions": null
-//    This will NOT be null, fill it in with organization and teams
-// }
-
-//   fetch(`//api.github.com/${userInput}`, { method: 'POST' })
-//       .then(response => {
-//         if (response.status >= 400) {
-//           // TODO: Do I need to pass this error to next()?
-//           throw new Error('Bad response from server');
-//         }
-//         return response.json();
-//       })
-//       .then(stories => {
-//         res.send(stories);
-//         return next();
-//       })
-//       .catch(next);
-// }
+fetch(`//api.github.com/repos/${userName}/${repoName}/branches/development/protection`, {
+  method: 'PUT',
+  'Content-Type': 'application/json',
+  body: {
+    required_status_checks: {
+      include_admins: true,
+      strict: true,
+      contexts: [
+        'continuous-integration/travis-ci', // Probably something different here
+      ],
+    },
+    required_pull_request_reviews: {
+      include_admins: false,
+    },
+    restrictions: null,
+     // This will NOT be null, fill it in with organization and teams
+  },
+});
